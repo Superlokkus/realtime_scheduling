e1269f5c4799faf440fe11b2c18628d603f606cc
Markus Klemm <markus@markusklemm.net>
2016-01-18 18:58:51 +0100

    Who needs std::chrono anyway

--- schedule_framesize/main.cpp
+++ schedule_framesize/main.cpp
@@ -9,9 +9,9 @@
 int main(int argc, const char * argv[]) {
     using std::cout; using std::endl;
     
-    cout << "Enter tasks like \nT<n>(<phi>,<p>,<e>,<d>)\nT<n>(<phi>,<p>,<e>,<d>)\n..." << endl;
+    cout << "Enter tasks like \nT<i>(<phi>,<p>,<e>,<d>)\nT<i+1>(<phi>,<p>,<e>,<d>)\n..." << endl;
     
-    periodic_task<> t1(1,6.0,1.0);
+    
     
     return 0;
 }
--- schedule_framesize/task.hpp
+++ schedule_framesize/task.hpp
@@ -4,31 +4,28 @@
 #ifndef SCHEDULE_FRAMESIZE_TASK_HPP
 #define SCHEDULE_FRAMESIZE_TASK_HPP
 
-#include <chrono>
 #include <iostream>
 
 /*!
  @brief Type to represent periodic tasks
  @tparam TimeRep Type of internal time representation
  */
-template <typename TimeRep = std::chrono::duration<double>, typename ID = unsigned>
+template <typename TimeRep>
 struct periodic_task {
     typedef TimeRep time_rep_t;
-    typedef ID id_t;
     
-    const ID i;       /*!< Index of task */
     TimeRep t_phi;    /*!< phase of task */
     TimeRep t_p;      /*!< period */
     TimeRep t_e;      /*!< execution time */
     TimeRep t_d;      /*!< deadline */
     
     
-    periodic_task(ID i,TimeRep t_phi, TimeRep t_p, TimeRep t_e, TimeRep t_d) :
-    i(i), t_phi(0), t_p(t_p), t_e(t_e), t_d(t_d)
+    periodic_task(TimeRep t_phi, TimeRep t_p, TimeRep t_e, TimeRep t_d) :
+    t_phi(0), t_p(t_p), t_e(t_e), t_d(t_d)
     {}
     
-    periodic_task(ID i, TimeRep t_p, TimeRep t_e) :
-    periodic_task(i,0,t_p,t_e,t_p)
+    periodic_task(TimeRep t_p, TimeRep t_e) :
+    periodic_task(TimeRep(0),t_p,t_e,t_p)
     {}
     
     
@@ -36,8 +33,8 @@ struct periodic_task {
 
 template <typename TimeRep>
 std::ostream& operator<< (std::ostream &os, const periodic_task<TimeRep>& task){
-    os << "T" << task.i << "(" << task.t_phi << " "
-    << task.t_p << " " << task.t_e << " " << task.t_d << ")";
+    os << "T" << "(" << task.t_phi << ","
+    << task.t_p << "," << task.t_e << "," << task.t_d << ")";
     
     return os;
 }
